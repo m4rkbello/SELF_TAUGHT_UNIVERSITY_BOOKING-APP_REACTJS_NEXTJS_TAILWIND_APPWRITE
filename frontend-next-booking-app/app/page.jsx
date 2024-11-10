421dc6bd3c6625dddcbce51b228beda05247c2a4
m4rkbello <markbello10041999@gmail.com>
2024-11-10 18:47:36 +0800

    ADD SOME FILES FROM TRAVERSY

--- frontend-next-booking-app/app/page.jsx
+++ frontend-next-booking-app/app/page.jsx
@@ -1,11 +1,20 @@
+import rooms from '@/data/rooms.json';
+
+
 
 
 export default function Home() {
   return (
-<>
-<h1>
-  Booking App
-</h1>
-</>
+    <>
+    
+        {rooms.length > 0 && rooms.length !== 0 ? (
+          rooms.map((room) => <h3>
+            {room.name}
+          </h3>)
+        ) : (
+         <p>No rooms available!</p>
+        )}
+     
+    </>
   );
 }
