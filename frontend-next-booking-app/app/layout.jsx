77108f6e2353614efc522c0bf8c3138d01d10824
m4rkbello <markbello10041999@gmail.com>
2024-11-10 15:15:14 +0800

    ADDED

--- frontend-next-booking-app/app/layout.jsx
+++ frontend-next-booking-app/app/layout.jsx
@@ -1,10 +1,11 @@
 import {Inter} from "next/font/google";
 import "@/assets/styles/globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: "BOOKING APP USING RANT STACK",
+  title: "BOOKING APP USING RANT STACK BY M4RKBELLO",
   description: "Book a meeting or conference room for your team.",
 };
 
@@ -14,10 +15,10 @@ export default function RootLayout({ children }) {
       <body
         className={inter.className}
       >
-         <main className="mx-ato max-w-7xl px-4 py-6">
-<h1>estT</h1>
-         </main>
+        <Header />
+         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         {children}
+         </main>
       </body>
     </html>
   );
